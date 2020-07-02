77906fbb1a46f78bd4481eaa9a73c626587c04c3
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-07-02 16:53:35 +0530

    sample view

--- modalExample.js
+++ modalExample.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
-import { StyleSheet, Dimensions } from "react-native";
+import { StyleSheet, Dimensions, Alert, Switch } from "react-native";
 import { Text, Modal, View, Button } from "react-native";
 import TextContent from "./textContent";
 
-const BasicModal = ({
+const ExampleBasicModal = ({
   description,
   openButtonTitle,
   closeButtonTitle,
   animationType,
+  transparent,
   hardwareAccelerated,
 }) => {
   const [visible, setVisible] = useState(false);
@@ -22,6 +23,7 @@ const BasicModal = ({
         visible={visible}
         animationType={animationType}
         hardwareAccelerated={hardwareAccelerated}
+        transparent={transparent}
       >
         <View style={styles.modal}>
           <TextContent />
@@ -32,11 +34,12 @@ const BasicModal = ({
   );
 };
 
-const StatusBarTranslucent = ({
+const ExampleStatusBarTranslucent = ({
   description,
   openButtonTitle,
   closeButtonTitle,
   animationType,
+  transparent,
   hardwareAccelerated,
 }) => {
   const [visible, setVisible] = useState(false);
@@ -53,6 +56,7 @@ const StatusBarTranslucent = ({
         visible={visible}
         animationType={animationType}
         hardwareAccelerated={hardwareAccelerated}
+        transparent={transparent}
         statusBarTranslucent={true}
       >
         <View
@@ -71,26 +75,298 @@ const StatusBarTranslucent = ({
   );
 };
 
-const App = () => {
+const ExampleOnDismiss = ({
+  description,
+  openButtonTitle,
+  closeButtonTitle,
+  animationType,
+  transparent,
+  hardwareAccelerated,
+}) => {
   const [visible, setVisible] = useState(false);
+
+  return (
+    <View style={styles.example}>
+      <Text>{description}</Text>
+      <View style={styles.openButton}>
+        <Button title={openButtonTitle} onPress={() => setVisible(true)} />
+      </View>
+      <Modal
+        visible={visible}
+        animationType={animationType}
+        hardwareAccelerated={hardwareAccelerated}
+        transparent={transparent}
+        onDismiss={() => {
+          Alert.alert(
+            "Welcome on board",
+            "Thanks for Accepting",
+            [
+              {
+                text: "Cancel",
+                onPress: () => console.log("Cancel Pressed"),
+              },
+              { text: "OK", onPress: () => console.log("OK Pressed") },
+            ],
+            { cancelable: false }
+          );
+        }}
+      >
+        <View style={styles.modal}>
+          <TextContent />
+          <Button onPress={() => setVisible(false)} title={closeButtonTitle} />
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+const ExampleOnOrientationChange = ({
+  description,
+  openButtonTitle,
+  closeButtonTitle,
+  animationType,
+  transparent,
+  hardwareAccelerated,
+}) => {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <View style={styles.example}>
+      <Text>{description}</Text>
+      <View style={styles.openButton}>
+        <Button title={openButtonTitle} onPress={() => setVisible(true)} />
+      </View>
+      <Modal
+        visible={visible}
+        animationType={animationType}
+        hardwareAccelerated={hardwareAccelerated}
+        transparent={transparent}
+        onOrientationChange={() => {
+          Alert.alert(
+            "Orientation Changed",
+            "See content in a different prospective",
+            [
+              {
+                text: "Cancel",
+                onPress: () => console.log("Cancel Pressed"),
+              },
+              { text: "OK", onPress: () => console.log("OK Pressed") },
+            ],
+            { cancelable: false }
+          );
+        }}
+      >
+        <View style={styles.modal}>
+          <TextContent />
+          <Button onPress={() => setVisible(false)} title={closeButtonTitle} />
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+const ExampleOnShow = ({
+  description,
+  openButtonTitle,
+  closeButtonTitle,
+  animationType,
+  transparent,
+  hardwareAccelerated,
+}) => {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <View style={styles.example}>
+      <Text>{description}</Text>
+      <View style={styles.openButton}>
+        <Button title={openButtonTitle} onPress={() => setVisible(true)} />
+      </View>
+      <Modal
+        visible={visible}
+        animationType={animationType}
+        hardwareAccelerated={hardwareAccelerated}
+        transparent={transparent}
+        onShow={() => {
+          Alert.alert(
+            "Welcome User",
+            "You can not move back from here. Proceed only when you're sure of it",
+            [
+              {
+                text: "Cancel",
+                onPress: () => setVisible(false),
+              },
+              { text: "OK", onPress: () => console.log("OK Pressed") },
+            ],
+            { cancelable: false }
+          );
+        }}
+      >
+        <View style={styles.modal}>
+          <TextContent />
+          <Button onPress={() => setVisible(false)} title={closeButtonTitle} />
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+const ExampleOnRequestClose = ({
+  description,
+  openButtonTitle,
+  closeButtonTitle,
+  animationType,
+  transparent,
+  hardwareAccelerated,
+}) => {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <View style={styles.example}>
+      <Text>{description}</Text>
+      <View style={styles.openButton}>
+        <Button title={openButtonTitle} onPress={() => setVisible(true)} />
+      </View>
+      <Modal
+        visible={visible}
+        animationType={animationType}
+        hardwareAccelerated={hardwareAccelerated}
+        transparent={transparent}
+        onRequestClose={() => {
+          Alert.alert(
+            "Are you sure",
+            "going back from this step is not recommended",
+            [
+              {
+                text: "Cancel",
+                onPress: () => console.log("Cancel Pressed"),
+              },
+              { text: "OK", onPress: () => console.log("OK Pressed") },
+            ],
+            { cancelable: false }
+          );
+        }}
+      >
+        <View style={styles.modal}>
+          <TextContent />
+          <Button onPress={() => setVisible(false)} title={closeButtonTitle} />
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+const ExamplePresentationStyle = ({
+  description,
+  openButtonTitle,
+  closeButtonTitle,
+  animationType,
+  transparent,
+  hardwareAccelerated,
+}) => {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <View style={styles.example}>
+      <Text>{description}</Text>
+      <View style={styles.openButton}>
+        <Button title={openButtonTitle} onPress={() => setVisible(true)} />
+      </View>
+      <Modal
+        visible={visible}
+        animationType={animationType}
+        hardwareAccelerated={hardwareAccelerated}
+        presentationStyle="formSheet"
+      >
+        <View style={styles.modal}>
+          <TextContent />
+          <Button onPress={() => setVisible(false)} title={closeButtonTitle} />
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+const App = () => {
   const [hardwareAccelerated, setHardwareAccelerated] = useState(false);
+  const [animation, setAnimation] = useState("slide");
+  const [transparent, setTransparent] = useState(false);
   return (
     <View style={styles.parentView}>
-      <BasicModal
-        description="Simple Read More Dialog"
-        openButtonTitle="Read More"
-        closeButtonTitle="Proceed"
-        animationType="slide"
-        hardwareAccelerated={hardwareAccelerated}
-      />
+      <View style={{ flex: 1, flexDirection: "row", marginBottom: 30 }}>
+        <View>
+          <Text>Transparent</Text>
+        </View>
+        <View>
+          <Switch
+            onValueChange={() => setTransparent(!transparent)}
+            value={transparent}
+          />
+        </View>
+      </View>
+      <View>
+        <ExampleBasicModal
+          description="Simple Read More Dialog"
+          openButtonTitle="Read More"
+          closeButtonTitle="Proceed"
+          animationType={animation}
+          hardwareAccelerated={hardwareAccelerated}
+          transparent={transparent}
+        />
 
-      <StatusBarTranslucent
-        description="Read important Content Covering all screen"
-        openButtonTitle="Open in Full Screen View"
-        closeButtonTitle="Proceed"
-        animationType="slide"
-        hardwareAccelerated={hardwareAccelerated}
-      />
+        <ExampleStatusBarTranslucent
+          description="Read important Content Covering all screen"
+          openButtonTitle="Open in Full Screen View"
+          closeButtonTitle="Proceed"
+          animationType={animation}
+          hardwareAccelerated={hardwareAccelerated}
+          transparent={transparent}
+        />
+
+        <ExampleOnDismiss
+          description="Please accept the terms and conditions"
+          openButtonTitle="View Terms and Conditions"
+          closeButtonTitle="Proceed"
+          animationType={animation}
+          hardwareAccelerated={hardwareAccelerated}
+          transparent={transparent}
+        />
+
+        <ExampleOnOrientationChange
+          description="Monitor orientation change in modal"
+          openButtonTitle="Open Modal"
+          closeButtonTitle="Proceed"
+          animationType={animation}
+          hardwareAccelerated={hardwareAccelerated}
+          transparent={transparent}
+        />
+
+        <ExampleOnRequestClose
+          description="Minitor Physical Back Button"
+          openButtonTitle="Open Modal"
+          closeButtonTitle="Proceed"
+          animationType={animation}
+          hardwareAccelerated={hardwareAccelerated}
+          transparent={transparent}
+        />
+
+        <ExampleOnShow
+          description="onShow"
+          openButtonTitle="Open Modal"
+          closeButtonTitle="Proceed"
+          animationType={animation}
+          hardwareAccelerated={hardwareAccelerated}
+          transparent={transparent}
+        />
+
+        <ExamplePresentationStyle
+          description="Presentation Mode"
+          openButtonTitle="Open Modal"
+          closeButtonTitle="Proceed"
+          animationType={animation}
+          hardwareAccelerated={hardwareAccelerated}
+          transparent={transparent}
+        />
+      </View>
     </View>
   );
 };
@@ -102,14 +378,15 @@ const styles = StyleSheet.create({
     marginRight: 20,
   },
   openButton: {
-    marginLeft: 20,
-    marginRight: 20,
+    marginLeft: 30,
+    marginRight: 30,
   },
   example: {
     marginBottom: 20,
   },
   modal: {
     padding: 20,
+    backgroundColor: "white",
   },
 });
 
