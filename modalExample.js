63e30925d650a6b78b8b72dfa2a26b9c12253700
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-07-07 01:54:18 +0530

    added text, modal builder integrated

--- modalExample.js
+++ modalExample.js
@@ -1,7 +1,17 @@
 import React, { useState } from "react";
-import { StyleSheet, Dimensions, Alert, Switch } from "react-native";
-import { Text, Modal, View, Button } from "react-native";
+import {
+  StyleSheet,
+  Dimensions,
+  Alert,
+  Switch,
+  ScrollView,
+  Text,
+  Modal,
+  View,
+  Button,
+} from "react-native";
 import TextContent from "./textContent";
+import ModalBuilder from "./builder.js";
 
 const ExampleBasicModal = ({
   description,
@@ -291,83 +301,94 @@ const App = () => {
   const [animation, setAnimation] = useState("slide");
   const [transparent, setTransparent] = useState(false);
   return (
-    <View style={styles.parentView}>
-      <View style={{ flex: 1, flexDirection: "row", marginBottom: 30 }}>
-        <View>
-          <Text>Transparent</Text>
+    <ScrollView>
+      <View style={styles.parentView}>
+        <View style={{ flex: 1, flexDirection: "row", marginBottom: 30 }}>
+          <View>
+            <Text>Make Examples Transparent : </Text>
+          </View>
+          <View>
+            <Switch
+              onValueChange={() => setTransparent(!transparent)}
+              value={transparent}
+            />
+          </View>
         </View>
         <View>
-          <Switch
-            onValueChange={() => setTransparent(!transparent)}
-            value={transparent}
+          <ExampleBasicModal
+            description="Simple Read More Dialog. A simple modal can be generated using 
+            the default configurations, triggered by a button press "
+            openButtonTitle="Read More"
+            closeButtonTitle="Proceed"
+            animationType={animation}
+            hardwareAccelerated={hardwareAccelerated}
+            transparent={transparent}
           />
+
+          <ExampleStatusBarTranslucent
+            description="Read important Content Covering all screen. Modals can be opened in multiple viewing modes, which are helpful in deciding modal behavior on large screens."
+            openButtonTitle="Open in Full Screen View"
+            closeButtonTitle="Proceed"
+            animationType={animation}
+            hardwareAccelerated={hardwareAccelerated}
+            transparent={transparent}
+          />
+
+          <ExampleOnDismiss
+            description="Please accept the terms and conditions. Few properties are only available for particular platforms, like listening for dismissAction"
+            openButtonTitle="View Terms and Conditions"
+            closeButtonTitle="Proceed"
+            animationType={animation}
+            hardwareAccelerated={hardwareAccelerated}
+            transparent={transparent}
+          />
+
+          <ExampleOnOrientationChange
+            description="Monitor orientation change in modal. Modals can listen for events or actions, for example change in orientation of device and trigger functions accordingly."
+            openButtonTitle="Open Modal"
+            closeButtonTitle="Proceed"
+            animationType={animation}
+            hardwareAccelerated={hardwareAccelerated}
+            transparent={transparent}
+          />
+
+          <ExampleOnRequestClose
+            description="Modals can be even watch for physical back button presses, attempts to close the modal and respond to such actions. This is done using onRequestClose prop."
+            openButtonTitle="Open Modal"
+            closeButtonTitle="Proceed"
+            animationType={animation}
+            hardwareAccelerated={hardwareAccelerated}
+            transparent={transparent}
+          />
+
+          <ExampleOnShow
+            description="You can even attach functions to modal show events. They are triggered as soon as the modal is opened. Checkout the example below"
+            openButtonTitle="Open Modal"
+            closeButtonTitle="Proceed"
+            animationType={animation}
+            hardwareAccelerated={hardwareAccelerated}
+            transparent={transparent}
+          />
+
+          <ExamplePresentationStyle
+            description="Presentation Mode : Modals can be configured to render differently, for example: to cover full screen, to appear at the center with side paddings etc "
+            openButtonTitle="Open Modal"
+            closeButtonTitle="Proceed"
+            animationType={animation}
+            hardwareAccelerated={hardwareAccelerated}
+            transparent={transparent}
+          />
+
+          <View style={{ paddingTop: 10 }}>
+            <Text>
+              Modal Playground : Here you can play around with modal props and
+              see live examples
+            </Text>
+            <ModalBuilder />
+          </View>
         </View>
       </View>
-      <View>
-        <ExampleBasicModal
-          description="Simple Read More Dialog"
-          openButtonTitle="Read More"
-          closeButtonTitle="Proceed"
-          animationType={animation}
-          hardwareAccelerated={hardwareAccelerated}
-          transparent={transparent}
-        />
-
-        <ExampleStatusBarTranslucent
-          description="Read important Content Covering all screen"
-          openButtonTitle="Open in Full Screen View"
-          closeButtonTitle="Proceed"
-          animationType={animation}
-          hardwareAccelerated={hardwareAccelerated}
-          transparent={transparent}
-        />
-
-        <ExampleOnDismiss
-          description="Please accept the terms and conditions"
-          openButtonTitle="View Terms and Conditions"
-          closeButtonTitle="Proceed"
-          animationType={animation}
-          hardwareAccelerated={hardwareAccelerated}
-          transparent={transparent}
-        />
-
-        <ExampleOnOrientationChange
-          description="Monitor orientation change in modal"
-          openButtonTitle="Open Modal"
-          closeButtonTitle="Proceed"
-          animationType={animation}
-          hardwareAccelerated={hardwareAccelerated}
-          transparent={transparent}
-        />
-
-        <ExampleOnRequestClose
-          description="Minitor Physical Back Button"
-          openButtonTitle="Open Modal"
-          closeButtonTitle="Proceed"
-          animationType={animation}
-          hardwareAccelerated={hardwareAccelerated}
-          transparent={transparent}
-        />
-
-        <ExampleOnShow
-          description="onShow"
-          openButtonTitle="Open Modal"
-          closeButtonTitle="Proceed"
-          animationType={animation}
-          hardwareAccelerated={hardwareAccelerated}
-          transparent={transparent}
-        />
-
-        <ExamplePresentationStyle
-          description="Presentation Mode"
-          openButtonTitle="Open Modal"
-          closeButtonTitle="Proceed"
-          animationType={animation}
-          hardwareAccelerated={hardwareAccelerated}
-          transparent={transparent}
-        />
-      </View>
-    </View>
+    </ScrollView>
   );
 };
 
